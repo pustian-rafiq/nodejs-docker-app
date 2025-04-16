8e7fabb7acc4c71bedd111b2b984dd19b827c820
pustian-rafiq <rafiqul.pust.cse@gmail.com>
2025-04-16 20:33:41 +0600

    Change code

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 4000;
 
-console.log('Request received v3');
+console.log('Request received via Docker!');
 // Define a simple route
 app.get('/', (req, res) => {
     res.send('Hello, World! From Docker!');
