f1dbaa5193fbbc19f7bb7c96409ed0b245f921e1
pustian-rafiq <rafiqul.pust.cse@gmail.com>
2025-04-16 20:28:03 +0600

    Docker and docker compose practice

---
+++ myapp/src/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+console.log('Request received v3');
+// Define a simple route
+app.get('/', (req, res) => {
+    res.send('Hello, World! From Docker!');
+});
+app.get('/user', (req, res) => {
+    
+    res.send({
+        name: 'John Doe',
+        age: 30,
+        email: 'rafiq@gmail.com',
+        phone: '1234567890',
+    });
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log('Database URL:', process.env.DATABASE_URL);
+    console.log(`Server is running on http://localhost:${port}`);
+});
